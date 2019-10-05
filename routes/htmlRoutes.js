efc49132aff4141c122d05462717931b18e2d36a
Alexander Militar <militara106@gmail.com>
2019-10-05 14:11:49 -0700

    main link fix

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,9 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
@@ -11,9 +11,55 @@ module.exports = function(app) {
     });
   });
 
+  // Calendar
+  app.get("/Calendar", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
+      res.render("calendar", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+
+  // Profile
+  app.get("/profile", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
+      res.render("profile", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+
+  // Nutrition
+  app.get("/nutrition", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
+      res.render("nutrition", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+
+
+  // Workout
+  app.get("/workout", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
+      res.render("workout", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+
+
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
@@ -21,7 +67,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
