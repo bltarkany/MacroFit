f74259a667ab57cbf75221b39f1f6fc2da368611
Alexander Militar <militara106@gmail.com>
2019-10-06 16:39:58 -0700

    intro page updated for styling

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,15 @@
 var db = require("../models");
 
-module.exports = function (app) {
-  // Load index page
-  app.get("/", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
+module.exports = function(app) {
+
+  // Intro Form
+  app.get("/", function(req, res) {
+    res.render("form",{layout:false});
+  });
+
+  // Load dashboard page
+  app.get("/dashboard", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
@@ -12,8 +18,8 @@ module.exports = function (app) {
   });
 
   // Calendar
-  app.get("/Calendar", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
+  app.get("/Calendar", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
       res.render("calendar", {
         msg: "Welcome!",
         examples: dbExamples
@@ -22,8 +28,8 @@ module.exports = function (app) {
   });
 
   // Profile
-  app.get("/profile", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
+  app.get("/profile", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
       res.render("profile", {
         msg: "Welcome!",
         examples: dbExamples
@@ -32,8 +38,8 @@ module.exports = function (app) {
   });
 
   // Nutrition
-  app.get("/nutrition", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
+  app.get("/nutrition", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
       res.render("nutrition", {
         msg: "Welcome!",
         examples: dbExamples
@@ -41,10 +47,9 @@ module.exports = function (app) {
     });
   });
 
-
   // Workout
-  app.get("/workout", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
+  app.get("/workout", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
       res.render("workout", {
         msg: "Welcome!",
         examples: dbExamples
@@ -52,14 +57,13 @@ module.exports = function (app) {
     });
   });
 
-
   // Load example page and pass in an example by id
-  app.get("/example/:id", function (req, res) {
+  app.get("/example/:id", function(req, res) {
     db.Example.findOne({
       where: {
         id: req.params.id
       }
-    }).then(function (dbExample) {
+    }).then(function(dbExample) {
       res.render("example", {
         example: dbExample
       });
@@ -67,7 +71,7 @@ module.exports = function (app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
+  app.get("*", function(req, res) {
     res.render("404");
   });
 };
